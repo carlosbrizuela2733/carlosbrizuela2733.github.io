22905c460a7dd33bfde3da6fb59a914d3c5a10d2
Carlos Norberto Brizuela <125180588+carlosbrizuela2733@users.noreply.github.com>
2024-02-27 22:43:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,30 +4,32 @@ firebase.initializeApp({
   authDomain: "turnos-3cb49.firebaseapp.com",
   projectId: "turnos-3cb49",
 });
+// Escuchar el evento 'DOMContentLoaded'
+document.addEventListener("DOMContentLoaded", function() {
+  var db = firebase.firestore();
 
-var db = firebase.firestore();
+  // Obtener el formulario y el campo de entrada de nombre
+  var formulario = document.getElementById("formulario");
+  var campoNombre = document.getElementById("nombre");
 
-// Obtener el formulario y el campo de entrada de nombre
-var vformulario = document.getElementById("formulario");
-var campoNombre = document.getElementById("nombre");
+  // Escuchar el evento de envío del formulario
+  formulario.addEventListener("submit", function(event) {
+    event.preventDefault(); // Evitar que el formulario se envíe
 
-// Escuchar el evento de envío del formulario
-vformulario.addEventListener("submit", function(event) {
-  event.preventDefault(); // Evitar que el formulario se envíe
+    var nombre = campoNombre.value; // Obtener el valor del campo de nombre
 
-  var nombre = campoNombre.value; // Obtener el valor del campo de nombre
-
-  // Agregar un documento a la colección "turnos" con el nombre ingresado
-  db.collection("turnos").add({
-    nombre: nombre,
-    estado: "en_espera",
-    fecha_hora: new Date().toISOString() // Usar la fecha y hora actual
-  })
-  .then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
-    campoNombre.value = ""; // Limpiar el campo de nombre después de agregar el registro
-  })
-  .catch(function(error) {
-    console.error("Error adding document: ", error);
+    // Agregar un documento a la colección "turnos" con el nombre ingresado
+    db.collection("turnos").add({
+      nombre: nombre,
+      estado: "en_espera",
+      fecha_hora: new Date().toISOString() // Usar la fecha y hora actual
+    })
+    .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+      campoNombre.value = ""; // Limpiar el campo de nombre después de agregar el registro
+    })
+    .catch(function(error) {
+      console.error("Error adding document: ", error);
+    });
   });
 });
