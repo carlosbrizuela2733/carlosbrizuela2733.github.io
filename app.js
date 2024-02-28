1e6329207df68a7278322f47ca93f721df0b3388
Carlos Norberto Brizuela <125180588+carlosbrizuela2733@users.noreply.github.com>
2024-02-27 22:33:44 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,16 +7,27 @@ firebase.initializeApp({
 
 var db = firebase.firestore();
 
-// Agregar un documento a la colección "turnos"
-db.collection("turnos")
-  .add({
-    nombre: "Otro",
-    estado: "pendiente",
-    fecha_hora: "08:00",
+// Obtener el formulario y el campo de entrada de nombre
+var formulario = document.getElementById("formulario");
+var campoNombre = document.getElementById("nombre");
+
+// Escuchar el evento de envío del formulario
+formulario.addEventListener("submit", function(event) {
+  event.preventDefault(); // Evitar que el formulario se envíe
+
+  var nombre = campoNombre.value; // Obtener el valor del campo de nombre
+
+  // Agregar un documento a la colección "turnos" con el nombre ingresado
+  db.collection("turnos").add({
+    nombre: nombre,
+    estado: "en_espera",
+    fecha_hora: new Date().toISOString() // Usar la fecha y hora actual
   })
-  .then(function (docRef) {
+  .then(function(docRef) {
     console.log("Document written with ID: ", docRef.id);
+    campoNombre.value = ""; // Limpiar el campo de nombre después de agregar el registro
   })
-  .catch(function (error) {
+  .catch(function(error) {
     console.error("Error adding document: ", error);
   });
+});
